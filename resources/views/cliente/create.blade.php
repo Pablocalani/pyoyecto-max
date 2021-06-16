58e419dc0684492adc73a8f79b1de99bc51aa73c
Pablocalani <javier2016r@hotmail.com>
2021-06-15 20:28:42 -0400

    agrege admin lte

--- resources/views/cliente/create.blade.php
+++ resources/views/cliente/create.blade.php
@@ -1,67 +1,98 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset ('css/bootstrap.min.css') }}">
-</head>
-<body> 
-  <div class="container"> 
-    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-      <div class="card-header">Formulario Cliente</div>
-      <div class="card-body">
-      <form action="dire">
-        <form>
-            <div class="form-row">
-              <div class="form-group col-md-6">
-                <label for="inputEmail4">URL destino</label>
-                <input type="url" class="form-control" placeholder="coloca URL de destino">
-              </div>
-              <div class="form-group col-md-6">
-                <label for="inputPassword4">Whatsapp</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="coloca tu Whatsapp">
-              </div>
-              <div class="form-group col-md-6">
-                <label for="inputPassword4">Nombre</label>
-                <input type="text" class="form-control" id="inputPassword4" placeholder="coloca tu Nombre">
-              </div>
+
+
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+<div class="container"> 
+  <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+    <div class="card-header">Formulario Cliente</div>
+    <div class="card-body">
+    <form action="dire">
+      <form>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="inputEmail4">URL destino</label>
+              <input type="url" class="form-control" placeholder="coloca URL de destino">
             </div>
-            <div class="form-group">
-              <label for="inputAddress">Referencia</label>
-              <input type="text" class="form-control" id="inputAddress" placeholder="Referencia del domicilio">
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Whatsapp</label>
+              <input type="text" class="form-control" id="inputPassword4" placeholder="coloca tu Whatsapp">
             </div>
-            <div class="form-row">
-              <label for="inputAddress">Cobrar</label>
-              <input type="text" class="form-control" id="inputAddress" placeholder="Monto a cobrar">
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Nombre</label>
+              <input type="text" class="form-control" id="inputPassword4" placeholder="coloca tu Nombre">
             </div>
-            <div class="form-group">
-              <label for="inputAddress2">Descripcion del Producto</label>
-              <input type="text" class="form-control" id="inputAddress2" placeholder="Descripcion del Producto">
+          </div>
+          <div class="form-group">
+            <label for="inputAddress">Referencia</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="Referencia del domicilio">
+          </div>
+          <div class="form-row">
+            <label for="inputAddress">Cobrar</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="Monto a cobrar">
+          </div>
+          <div class="form-group">
+            <label for="inputAddress2">Descripcion del Producto</label>
+            <input type="text" class="form-control" id="inputAddress2" placeholder="Descripcion del Producto">
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="inputCity">Ciudad</label>
+              <input type="text" class="form-control" id="inputCity" placeholder="cual es tu ciudad">
             </div>
-            <div class="form-row">
-              <div class="form-group col-md-6">
-                <label for="inputCity">Ciudad</label>
-                <input type="text" class="form-control" id="inputCity" placeholder="cual es tu ciudad">
-              </div>
-              <div class="form-group col-md-6">
-                <label for="inputCity">eres hombre o mujer</label>
-                <input type="text" class="form-control" id="inputCity" placeholder="eres hombre o mujer">
-              </div>
-              <div class="form-group col-md-2">
-                <label for="inputZip">Zip</label>
-                <input type="text" class="form-control" id="inputZip">
-              </div>
-            <div class="form-group">
+            <div class="form-group col-md-6">
+              <label for="inputCity">eres hombre o mujer</label>
+              <input type="text" class="form-control" id="inputCity" placeholder="eres hombre o mujer">
             </div>
-            <button type="submit" class="btn btn-primary mt-3">Aceptar</button>
-          </form>
-    </form>
-      </div>
+            <div class="form-group col-md-2">
+              <label for="inputZip">Zip</label>
+              <input type="text" class="form-control" id="inputZip">
+            </div>
+          <div class="form-group">
+          </div>
+          <button type="submit" class="btn btn-primary mt-3">Aceptar</button>
+        </form>
+  </form>
     </div>
   </div>
-    
-    </div>
-</body>
-</html>
+</div>
+  
+  </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
